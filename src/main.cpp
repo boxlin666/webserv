bcf3520cba2caf60f0a8651ac4695b8c9b8f0030
boxlin666 <boxlin666@gmail.com>
2026-04-10 13:01:47 +0200

    chore(formatter): add .clang-format file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
     if (argc > 2) {
         std::cerr << "Usage: ./webserv [config_file]" << std::endl;
         return 1;
