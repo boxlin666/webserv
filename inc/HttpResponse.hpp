af31c5ddd991107c3b992a8409fbedeb41c2c108
yan zhao <yanzhao@f4r5s6.paris.42.school>
2026-04-20 20:02:16 +0200

    feat(request): refine check request logic
    <body>
    
    <footer>

--- inc/HttpResponse.hpp
+++ inc/HttpResponse.hpp
@@ -10,11 +10,14 @@
 #define MAX_HEADER_SIZE 8192
 #define URI_TOO_LONG 414
 #define MAX_HEADER_SIZE 431
+#define SERVER_ERROR 500
 #define NO_HTTP_VERSION 505
 #define NO_METHOD 501
 #define BODY_TOO_LARGE 413
 #define PER_DENIED 403
 #define NOT_FOUND 404
+#define BAD_REQUEST 400
+#define SUCCESS 200
 
 class HttpResponse
 {
@@ -23,6 +26,7 @@ private:
     std::map<std::string, std::string> _headers;
     std::string _body;
     std::string _full_response;
+    std::size_t body_len;
     const ServerConfig* _config;
 
     HttpResponse(const HttpResponse& other);
@@ -32,8 +36,11 @@ private:
     void _handle_post();
     void _handle_delete();
 
-    int _check_request(const HttpRequest& req);
+    int _check_request(const HttpRequest& req)const;
+    std::string &_build_full_path(const HttpRequest& req, const ServerConfig& config)const;
+    int _check_resource(std::string &full_path);
 
+    void set_body_len(size_t input);
     // 根据文件后缀判断类型
     //std::string _get_content_type(std::string path);
 public:
--- src/http/HttpResponse.cpp
+++ src/http/HttpResponse.cpp
@@ -3,6 +3,10 @@
 #include <sstream>
 #include <iterator>
 
+#include <unistd.h> //tmp header missing config data 
+#include <sys/stat.h> 
+
+
 HttpResponse::HttpResponse(void)
 {
 
@@ -16,18 +20,28 @@ HttpResponse::~HttpResponse(void)
 void HttpResponse::build(const HttpRequest& req, const ServerConfig& config)
 {
     int ret;
+    std::string full_path;
 
     //1.prerequiste check! 
     ret = this->_check_request(req);
-    if (ret)
+    if (ret != 200)
     {
         this->set_status(ret);
         return ;
     }
 
     //2. path convert
+    full_path = this->_build_full_path(req, config);
  
     //3. check ressource (accessbility)
+    ret = this->_check_resource(full_path);
+
+    if (ret != 200)
+    {
+        this->set_status(ret);
+        return ;
+    }
+
 
     //4. get_raw_data
 
@@ -35,10 +49,11 @@ void HttpResponse::build(const HttpRequest& req, const ServerConfig& config)
 
 }
 
-int HttpResponse::_check_request(const HttpRequest& req)
+int HttpResponse::_check_request(const HttpRequest& req)const
 {
     size_t  req_line_len = 0;
     size_t  req_header_len = 0;
+    std::string request_path = "";
 
     req_line_len = req.get_method().length() + req.get_path().length() + req.get_http_version().length();
 
@@ -56,12 +71,75 @@ int HttpResponse::_check_request(const HttpRequest& req)
         return (NO_METHOD);
     if (req.get_body_len() > 100000000000000000 && req.get_method() == "POST") //temporary limit should be set up by config file!
         return (BODY_TOO_LARGE);
+    request_path = req.get_path();
+    if (request_path[0] != '/' || request_path.empty())
+        return (BAD_REQUEST);
+    /* TO DO
+        if find /../../ in path we return BAD_REQUEST as well
+    */ 
     return (200); //assume the check is OK! But we can still have 403 (permission denied) 404 （not found）
+}
+
+std::string &HttpResponse::_build_full_path(const HttpRequest& req, const ServerConfig& config)const
+{
+    //we assume _current_path ending without "\" (alreamd trimmed in Config class!)
+    std::string root_path; //should be found in config!
+    std::string full_path;
+    char    buffer[100]; //tmp 
+
+    getcwd(buffer, 100); //tmp because we don't have config!
+    root_path = buffer; //tmp
+    full_path = root_path + req.get_path(); //tmp funct!
+
+    return (full_path);
+}
+
+int HttpResponse::_check_resource(std::string &full_path)
+{
+    struct stat st;
+    struct stat st_index;
+
+    if (stat(full_path.c_str(), &st) == -1)
+    {
+        if (errno == ENONET)
+            return (NOT_FOUND);
+        else if (errno == EACCES)
+            return (PER_DENIED);
+        return (SERVER_ERROR);
+    }
+
+    if (S_ISDIR(st.st_mode))
+    {
+        if (full_path.back() != '/')
+            full_path += "/";
+        full_path += "index.html";
+        if (stat(full_path.c_str(), &st_index) == -1)
+        {
+            if (errno == ENOENT)
+                return (NOT_FOUND);
+            return (PER_DENIED);
+        }
+        if (!S_ISREG(st_index.st_mode))
+           return (NOT_FOUND); 
+        this->set_body_len(st_index.st_size);
+        return (SUCCESS);
+    }
+    if (S_ISREG(st.st_mode))
+    {
+        this->set_body_len(st.st_size);
+        return (SUCCESS);
+    }
+    return (NOT_FOUND);
 }
 
 void HttpResponse::set_status(int code)
 {
     this->_status_code = code;
+}
+
+void HttpResponse::set_body_len(size_t input)
+{
+    this->body_len = input;
 }
 
 void HttpResponse::add_header(const std::string& key, const std::string& value)
