50f0a1e5b53aa0fbcbac2ed6279d445a3ca54d73
yanzhao <zhaoyan19950101@gmail.com>
2026-05-19 22:31:54 +0200

    refactor(RequestHandler & Response): refactor logic of resource check in RequestHandler with CGI flag
    <body>
    
    <footer>

--- src/http/HttpResponseDefault.cpp
+++ src/http/HttpResponseDefault.cpp
@@ -20,11 +20,13 @@ void    HttpResponse::_init_status_msg_map(void)
     HttpResponse::_status_msg_map[BAD_REQUEST] = "Bad Request";
     HttpResponse::_status_msg_map[PER_DENIED] = "Permission Denied"; 
     HttpResponse::_status_msg_map[NOT_FOUND] = "Forbidden";
-
-    /* 5xx */
     HttpResponse::_status_msg_map[METHOD_NOT_ALLOWED] = "Method Not Allowed";
+    HttpResponse::_status_msg_map[NO_LENGTH] = "Length Required";
     HttpResponse::_status_msg_map[BODY_TOO_LARGE] = "Content Too Large";
     HttpResponse::_status_msg_map[URI_TOO_LONG] = "URI Too Long";
+    HttpResponse::_status_msg_map[REQ_HEADER_TOO_LONG] = "Request Header Fields Too Large";
+
+    /* 5xx */
     HttpResponse::_status_msg_map[SERVER_ERROR] = "Internal Server Error";
     HttpResponse::_status_msg_map[NO_METHOD] = "Not Implemented";
     HttpResponse::_status_msg_map[NO_HTTP_VERSION] = "HTTP Version Not Supported";
