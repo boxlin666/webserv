a4f00ef9230875c9c77e0a2f56f5db10eb2a93d0
yanzhao <zhaoy@LAPTOP-R9C8FOTQ.localdomain>
2026-06-18 02:43:16 +0200

    fix(server config): reinforce check for double path found in server config file

--- inc/Location.hpp
+++ inc/Location.hpp
@@ -13,20 +13,35 @@ struct location {
     std::string                root;
     std::map<int, std::string> error_pages;
     bool                       autoindex;
-
-    // access
+ 
     std::vector<std::string> methods;
     std::size_t              client_max_body_size;
 
-    int         return_code;
-    std::string return_url;
+    int                      return_code;
+    std::string              return_url;
 
-    // 处理配置文件里写的扩展指令
     std::string upload_path;
     std::string cgi_path;
     std::string cgi_ext;
     std::string cgi_script;
     std::string index_file;
+
+    location()
+        : _prefix(""),
+          index(),
+          root(""),
+          error_pages(),
+          autoindex(false),              
+          methods(),
+          client_max_body_size(0),   
+          return_code(0),              
+          return_url(""),
+          upload_path(""),
+          cgi_path(""),
+          cgi_ext(""),
+          cgi_script(""),
+          index_file("")
+    {}
 };
 
 #endif
